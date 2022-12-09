36c4368b97fe0ae50eb4e7967f72cad093e23d4f
Kevin van Nieukerke <k.v.nieukerke@gmail.com>
2022-12-09 11:36:36 +0100

    changed adapter from netlify to auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,8 @@
-import adapter from '@sveltejs/adapter-netlify';
-
+import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: true,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false,
-		}),
-		files: {
-			serviceWorker: 'src/service-worker.js',
-		},
+		adapter: adapter(),
 	},
 };
-
 export default config;
