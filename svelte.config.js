8a45c28346175cbbc5260b16d9e27a61ad22c4e5
Kevin van Nieukerke <k.v.nieukerke@gmail.com>
2022-12-09 11:43:57 +0100

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: true,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false,
+		}),
+		files: {
+			serviceWorker: 'src/service-worker.js',
+		},
 	},
 };
+
 export default config;
